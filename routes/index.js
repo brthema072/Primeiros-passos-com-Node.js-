f7320a3ad21bfee92b455a85e8c45ceb8d317a80
bruno_marthe <brunomarthe@hotmail.com>
2019-09-03 20:48:39 -0300

    parte 2

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Olá Mundo' });
 });
 
+/* GET new page. */
+router.get('/new', function(req, res, next) {
+  res.render('new', { title: 'Novo Cadastro' });
+});
+
+/* POST new page */
+router.post('/new', function(req, res, next){
+  var nome = req.body.nome;
+  var idade = req.body.idade;
+  res.redirect('/?nome='+ nome+ '?idade='+ idade);
+});
 module.exports = router;
